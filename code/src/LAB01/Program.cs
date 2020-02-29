31364d0b5e78501f199edfe8e9df3aa7a0dfaa5c
Jean oliveira <JeanoliveiraJeansilva@hotmail.com>
2020-02-29 11:17:20 -0300

    first commit

---
+++ code/src/LAB01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LAB01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            //Concatenação
+            string resultado;
+            var data = DateTime.Now;
+
+            resultado = "A" + "B";
+            resultado = string.Concat("A","B","C","D");
+            resultado = string.Format("Olá: {0}","Jean");
+            resultado = $"Hora: {data.ToString("dd/MM/yyyy HH:mm")}";
+
+            Console.WriteLine(resultado);
+
+            //Operadores
+            int num, num2; 
+            Console.WriteLine("Digite um número: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite outro número");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"O resultado da soma é: {num + num2}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
