d1e472d0915fb5b2970fa66d8a379fcf50a53da4
Jean oliveira <JeanoliveiraJeansilva@hotmail.com>
2020-03-14 17:00:48 -0300

    .

--- code/src/LAB08-CONSUMO/Form1.Designer.cs
+++ code/src/LAB08-CONSUMO/Form1.Designer.cs
@@ -29,34 +29,93 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnAddAluno = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxNome = new System.Windows.Forms.TextBox();
+            this.textBoxCPF = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 118);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 93);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(776, 170);
             this.dataGridView1.TabIndex = 0;
+            // 
+            // btnAddAluno
+            // 
+            this.btnAddAluno.Location = new System.Drawing.Point(12, 306);
+            this.btnAddAluno.Name = "btnAddAluno";
+            this.btnAddAluno.Size = new System.Drawing.Size(201, 48);
+            this.btnAddAluno.TabIndex = 1;
+            this.btnAddAluno.Text = "Adicionar";
+            this.btnAddAluno.UseVisualStyleBackColor = true;
+            this.btnAddAluno.Click += new System.EventHandler(this.btnAddAluno_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nome:";
+            // 
+            // textBoxNome
+            // 
+            this.textBoxNome.Location = new System.Drawing.Point(57, 10);
+            this.textBoxNome.Name = "textBoxNome";
+            this.textBoxNome.Size = new System.Drawing.Size(187, 20);
+            this.textBoxNome.TabIndex = 3;
+            // 
+            // textBoxCPF
+            // 
+            this.textBoxCPF.Location = new System.Drawing.Point(57, 36);
+            this.textBoxCPF.MaxLength = 14;
+            this.textBoxCPF.Name = "textBoxCPF";
+            this.textBoxCPF.Size = new System.Drawing.Size(115, 20);
+            this.textBoxCPF.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "CPF:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxCPF);
+            this.Controls.Add(this.textBoxNome);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAddAluno);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnAddAluno;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxNome;
+        private System.Windows.Forms.TextBox textBoxCPF;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- code/src/LAB08-CONSUMO/Form1.cs
+++ code/src/LAB08-CONSUMO/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LAB08_CONSUMO.AlunoService;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -25,6 +26,19 @@ namespace LAB08_CONSUMO
         private void PreencherLista()
         {
             AlunoService.AlunoServiceClient client = new AlunoService.AlunoServiceClient();
+
+            dataGridView1.DataSource = client.GetAlunos(true);
+        }
+
+        private void btnAddAluno_Click(object sender, EventArgs e)
+        {
+            AlunoServiceClient client = new AlunoServiceClient();
+
+            Aluno aluno = new Aluno();
+            aluno.CPF = textBoxCPF.Text;
+            aluno.Nome = textBoxNome.Text;
+
+            client.AddStudent(aluno);
 
             dataGridView1.DataSource = client.GetAlunos(true);
         }
